322aa2ed0507b6292959699636584ea3d2ab1a4d
Robson Magalhães <robsonom92@hotmail.com>
2026-07-27 16:20:11 -0300

    ok

--- api/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$_SERVER['SCRIPT_FILENAME'] = getcwd() . '/public/index.php';
-chdir(__DIR__ . '/..');
-
-require __DIR__ . '/../public/index.php';
--- public/index.php
+++ public/index.php
@@ -1,19 +1,28 @@
 <?php
 
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+register_shutdown_function(function () {
+    $error = error_get_last();
+    if ($error !== null) {
+        header('Content-Type: text/plain');
+        echo "FATAL ERROR CAPTURED:\n";
+        echo print_r($error, true);
+    }
+});
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
